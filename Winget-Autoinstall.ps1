93bec3b2e63113ad8bb1462a30c7bc125de14bbb
lukasuvinha <47397238+lukasuvinha@users.noreply.github.com>
2023-01-26 14:39:43 -0300

    Update Winget-Autoinstall.ps1
    Segunda versão beta do script de auto instalação para windows. 
    
    Bloco de verificação de privilegios foi desativado por estar encerrando o script logo após solicitar o administrador, no aguardo de correção.
    
    Adicionado linha que instala o Java Runtime
    
    Adicionado linha que instala o KeePassXC
    
    Adicionado linha que instala o KeePass (a redundância em relação ao KeePassXC se faz necessária no contexto especifico para qual este script foi escrito, KeePassXC pode não exibir sua janela dependendo do tipo de acesso remoto feito a maquina quando necessário, trazendo a necessidade de ter o KeePass para ser possível usar o banco de senhas via acesso remoto [Anydesk, no caso especifico deste script], se não houver a necessidade é aconselhável adicionar o caractere de comentário "#" no inicio da linha do KeePass para desativar sua instalação).
    
    Refinado o bloco de comentário que especifica quais são os programas instalados via msstore e adicionado uma observação sobre o pacote do office 365, existem duas linhas que instalam o mesmo pacote dos dois repositórios (winget e msstore ), o definido como padrão é o pacote via winget, em caso de problemas de compatibilidade ou melhorias futuras do script, a opção via msstore vai se encontrar no script desativada.
    
    Algumas linhas foram realocadas no proposito de deixar o código "palpável" aos olhos, isto não altera o comportamento final do script.

--- Winget-Autoinstall.ps1
+++ Winget-Autoinstall.ps1
@@ -1,14 +1,17 @@
 #!powershell
 
+
 <### BLOCO QUE VERIFICA PRIVILEGIOS DE ADMINISTRADOR ###>
-$id = [System.Security.Principal.WindowsIdentity]::GetCurrent()
-$principal = New-Object System.Security.Principal.WindowsPrincipal($id)
-if (!$principal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)) {
-Write-Host "Este script precisa ser executado com privilégios de administrador."
-$arguments = "& '" + $myinvocation.mycommand.definition + "'"
-Start-Process powershell -Verb runAs -ArgumentList $arguments
-exit
-}
+<# 
+    $id = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+    $principal = New-Object System.Security.Principal.WindowsPrincipal($id)
+    if (!$principal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Write-Host "Este script precisa ser executado com privilégios de administrador."
+    $arguments = "& '" + $myinvocation.mycommand.definition + "'"
+    Start-Process powershell -Verb runAs -ArgumentList $arguments
+    exit
+    }
+#>
 
 <### LIBERAR POWERSHELL PARA EXECUÇÃO DE SCRIPTS ###>
 Set-ExecutionPolicy Unrestricted -Scope Process
@@ -17,7 +20,6 @@ Set-ExecutionPolicy Unrestricted -Scope Process
 winget install revouninistaller.revouninstaller --accept-package-agreements --accept-source-agreements
 winget install AnyDesk --force --accept-package-agreements --accept-source-agreements
 winget install Microsoft.Office --accept-package-agreements --accept-source-agreements
-#winget install 9WZDNCRD29V9 --accept-package-agreements --accept-source-agreements
 winget install Google.Chrome --accept-package-agreements --accept-source-agreements
 winget install Mozilla.Firefox --accept-package-agreements --accept-source-agreements
 winget install Opera.Opera --accept-package-agreements --accept-source-agreements
@@ -27,6 +29,7 @@ winget install 9NKSQGP7F2NH --accept-package-agreements --accept-source-agreemen
 winget install 9N0DX20HK701 --accept-package-agreements --accept-source-agreements
 winget install XPDP273C0XHQH2 --accept-package-agreements --accept-source-agreements
 winget install 9WZDNCRFHWGG --accept-package-agreements --accept-source-agreements
+winget install Oracle.JavaRuntimeEnvironment --accept-package-agreements --accept-source-agreements
 winget install Microsoft.VCRedist.2008.x64 --accept-package-agreements --accept-source-agreements
 winget install Microsoft.VCRedist.2010.x64 --accept-package-agreements --accept-source-agreements
 winget install Microsoft.VCRedist.2013.x64 --accept-package-agreements --accept-source-agreements
@@ -35,8 +38,17 @@ winget install Microsoft.VCRedist.2012.x64 --accept-package-agreements --accept-
 winget install Microsoft.DotNet.SDK.6 --accept-package-agreements --accept-source-agreements
 winget install Microsoft.DotNet.SDK.5 --accept-package-agreements --accept-source-agreements
 winget install Microsoft.DotNet.SDK.3_1 --accept-package-agreements --accept-source-agreements
+#winget install 9WZDNCRD29V9 --accept-package-agreements --accept-source-agreements
+#winget install KeePassXCTeam.KeePassXC --accept-package-agreements --accept-source-agreements
+#winget install dominikreichl.keepass --accept-package-agreements --accept-source-agreements
 
-
-<# 9NKSQGP7F2NH, 9N0DX20HK701, XPDP273C0XHQH2 E 9WZDNCRFHWGG SÃO O WHATSAPP, WINDOWS TERMINAL, ADOBE ACROBAT E SAMSUNG PRINTER EXPERIENCE RESPECTIVAMENTE DIRETO DA MSSTORE #>
+<#
+    PACOTES DE ORIGEM DA MSSTORE
+    9NKSQGP7F2NH = WHATSAPP
+    9N0DX20HK701 = WINDOWS TERMINAL
+    XPDP273C0XHQH2 = ADOBE ACROBAT
+    9WZDNCRFHWGG =  SAMSUNG PRINTER EXPERIENCE
+    9WZDNCRD29V9 = Office 365 ( ! DESATIVADO ! - script já está baixando o mesmo do WINGET)
+#>
 
 pause
